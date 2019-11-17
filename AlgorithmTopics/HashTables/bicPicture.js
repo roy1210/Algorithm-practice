03e479abeb7f058c2cfdbf31adc1997ff4a64b65
”roy1210” <rswkure@gmail.com>
2019-11-18 07:11:02 +0800

    Added topics for array and leetcode

---
+++ AlgorithmTopics/HashTables/bicPicture.js
@@ -0,0 +1,3 @@
+// Key: Value
+// Can access the value like `basket.grapes = 1000`
+// insert, lookup, delete, search, all is O(1)
---
+++ LeetCode/217-containsDuplicate.js
@@ -0,0 +1,30 @@
+// Given an array of integers, find if the array contains any duplicates.
+
+// Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+// Example 1:
+
+// Input: [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+var containsDuplicate = function(nums) {};
+
+// var containsDuplicate = function(nums) {
+//   var hash = {};
+//   for (var i = nums.length - 1; i >= 0; i--) {
+//     if (hash[nums[i]]) {
+//       return true;
+//     } else {
+//       hash[nums[i]] = 1;
+//     }
+//   }
+//   return false;
+// };
